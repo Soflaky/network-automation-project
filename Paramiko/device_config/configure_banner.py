a35c9403311a2c1232b6d77a1957a7d8f0932df6
AndyKwan47 <kwanzhiren@graduate.utm.my>
2026-06-20 04:10:12 +0000

    Add Kwan banner configuration script

---
+++ Paramiko/device_config/configure_banner.py
@@ -0,0 +1,22 @@
+from kwan_ssh_helper import run_device_commands
+
+
+def main():
+    config_commands = [
+        "banner motd #UNAUTHORIZED ACCESS PROHIBITED - CONFIGURED BY KWAN#"
+    ]
+
+    verify_commands = [
+        "show running-config | include banner"
+    ]
+
+    run_device_commands(
+        config_commands=config_commands,
+        verify_commands=verify_commands,
+        output_filename="banner_config_output.txt"
+    )
+
+
+
+if __name__ == "__main__":
+    main()
